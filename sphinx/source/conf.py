cd891396e700b6d1ab1754504e97feeb4694d99d
Niek Tax <niektax@gmail.com>
2025-12-08 06:31:05 -0800

    Add COPYRIGHT files (#25)
    Summary:
    Added more copyright headers, based on automated repo check report
    
    Pull Request resolved: https://github.com/facebookincubator/MCGrad/pull/25
    
    Reviewed By: Lorenzo-Perini
    
    Differential Revision: D88515308
    
    Pulled By: TaXxER
    
    fbshipit-source-id: f587a21f60cdcb3e1de5755f6d39f6d0af62f05f

--- sphinx/source/conf.py
+++ sphinx/source/conf.py
@@ -1,3 +1,8 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
 # Configuration file for the Sphinx documentation builder.
 #
 # For the full list of built-in configuration values, see the documentation:
